d71c574ba034399fea84eda42313e857d56b6dc3
Jeffron <jeffroneu@gmail.com>
2020-01-13 21:23:11 +0100

    fixed registration form in redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
 import { GET_ERRORS } from "./types";
+import { SET_CURRENT_USER } from "./types";
 
 // REGISTERING USER
 
@@ -13,4 +17,37 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login User
+export const LoginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login")
+    .then(res => {
+      // save to local storage
+      const { token } = res.data;
+      // save token to local storage
+      localStorage.setItem("jwtToken", token);
+      // set token to header
+      setAuthToken(token);
+      // decode token to get user date
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set current user
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
